143661c85e0c78bcb45fc2e7d263b8a827d0306c
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-18 23:48:13 +0600

    first commit

---
+++ class1.js
@@ -0,0 +1,21 @@
+//can you find out who will get the strawberry?
+const disha = 55;
+const misha = 60;
+if(disha>misha){
+    console.log('disha will get this')
+}
+else{
+    console.log('misha will get this')
+}
+
+function getReward(a, b){
+    if(a>b){
+    console.log('disha will get this')
+}
+else{
+    console.log('misha will get this')
+}
+}
+const i = 5;
+const j = 9;
+console.log(getReward(i, j),'reward')
