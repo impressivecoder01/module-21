9d6e9b3c58cc214e8bdeb04bbeca36fbbbadb408
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-20 23:37:17 +0600

    .

--- class3.js
+++ class3.js
@@ -1,17 +1,18 @@
 //use add and multiplication to calculate wood requirements
-const woodPerChair = 3;   // 3 cubic feet per chair
-const woodPerTable = 10;  // 10 cubic feet per table
-const woodPerBed = 15;    // 15 cubic feet per bed
+// const woodPerChair = 3;  
+// const woodPerTable = 10;  
+// const woodPerBed = 15;   
 
-// Number of items ordered
-const numChairs = 4;
-const numTables = 2;
-const numBeds = 1;
 
-// Total wood = (wood per item × quantity) + ...
-const totalWood =
-  (woodPerChair * numChairs) +
-  (woodPerTable * numTables) +
-  (woodPerBed * numBeds);
+// const numChairs = 4;
+// const numTables = 2;
+// const numBeds = 1;
 
-console.log("Total wood required: " + totalWood + " cubic feet");
+// const totalWood =
+//   (woodPerChair * numChairs) +
+//   (woodPerTable * numTables) +
+//   (woodPerBed * numBeds);
+
+// console.log("Total wood required: " + totalWood + " cubic feet");
+
+//code form online.
