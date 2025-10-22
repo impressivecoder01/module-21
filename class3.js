08449a3d69d3a60cbe7c1459d98dc11dbaacab3e
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-22 22:20:08 +0600

    class3Done

--- class3.js
+++ class3.js
@@ -16,3 +16,39 @@
 // console.log("Total wood required: " + totalWood + " cubic feet");
 
 //code form online.
+
+// chair --> 3cft 
+// table --> 10cft 
+// baed  --> 50cft
+
+function woodQuantity(chair, table, bed){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+    const allChairWood = chair * perChairWood;
+    const allTableWood = table * perTableWood;
+    const allBedWood = bed * perBedWood;
+    const totalWood = allBedWood+allChairWood+allTableWood;
+    return totalWood;
+}
+const t = 3;
+const c = 3;
+const b = 3;
+const now = woodQuantity(t,c,b);
+console.log(now);
+
+// shirt price --> 500;
+// pant price --> 300;
+// shoe price --> 900;
+function fashion(shirt,pant,shoe){
+    const perShirtPrice = 500;
+    const perPantPrice = 400;
+    const perShoePrice = 900;
+    const nowShirtPrice = perShirtPrice * shirt;
+    const nowPantPrice = perPantPrice * pant;
+    const nowShoePrice = perShoePrice * shoe;
+    const total = nowPantPrice + nowShirtPrice + nowShoePrice;
+    return total;
+}
+const nowFashion = fashion(2,2,2);
+console.log(nowFashion)
