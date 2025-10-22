43997f5b1085751905d333d8fa65e7bc6c0c0372
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-23 00:01:26 +0600

    totalPricecalculatebyMe

--- class5.js
+++ class5.js
@@ -1,1 +1,14 @@
 //calculate the total cost of the products in a shopping cart
+const products = [
+    {name: 'shampoo', price : 300},
+    {name: 'shampoo2', price : 400},
+    {name: 'shampoo3', price : 500}
+]
+function totalPrice(array){
+    let sum = 0;
+    for(let ary of array){
+        sum = ary.price + sum;
+    }
+    return sum;
+}
+console.log(totalPrice(products))
