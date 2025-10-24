154fab02ae820e4b5902eaa0e8a4e679bd965e38
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-24 23:42:50 +0600

    \

--- class6.js
+++ class6.js
@@ -25,3 +25,37 @@ console.log(discountPrice(num))
 console.log(discountPrice(num1))
 console.log(discountPrice(num2))
 console.log(discountPrice(num3))
+
+// ph code
+function discount(quantity){
+
+}
+//chatgpt code
+// function multiLayerDiscount(price, d1, d2, d3) {
+//   let finalPrice = price;
+
+//   if (d1 > 0) {
+//     finalPrice = finalPrice - (finalPrice * d1 / 100);
+//   } else {
+//     finalPrice = finalPrice;
+//   }
+
+//   if (d2 > 0) {
+//     finalPrice = finalPrice - (finalPrice * d2 / 100);
+//   } else {
+//     finalPrice = finalPrice;
+//   }
+
+//   if (d3 > 0) {
+//     finalPrice = finalPrice - (finalPrice * d3 / 100);
+//   } else {
+//     finalPrice = finalPrice;
+//   }
+
+//   return parseFloat(finalPrice.toFixed(2));
+// }
+
+
+// const originalPrice = 1000;
+// const finalPrice = multiLayerDiscount(originalPrice, 10, 5, 3);
+// console.log("Final Price: $" + finalPrice);
