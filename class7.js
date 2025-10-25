63ea6af7bbf21d6640efb299e16e309f1176caf2
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-25 22:54:53 +0600

    simple calculator class7

--- class7.js
+++ class7.js
@@ -1,1 +1,30 @@
 //simple calculator to sell function inside a function
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1 , num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+function calculator(a,b, operation){
+    if(operation === 'add'){
+        return add(a, b)
+    }
+    else if (operation === 'subtract'){
+        return subtract(a,b)
+    }
+    else if (operation === 'multiply'){
+        return multiply(a,b)
+    }
+    else {
+        return 'no operational option found'
+    }
+}
+console.log(calculator(3,3,'add'))
