2168d80c0dbeb057a94daa25193572e3eeda1205
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-26 22:58:04 +0600

    verification

--- class8.js
+++ class8.js
@@ -1,1 +1,12 @@
 //handle unexpected function input parameter error
+function multiply (num1, num2){
+    if(typeof num1 && typeof num2 === 'number'){
+
+        const mult = num1 * num2;
+        return mult;
+    }
+}
+const result = multiply(4,4);
+console.log(result)
+// let a = 5;
+// console.log(typeof a)
