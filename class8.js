d4b9e66d5c8c996a9f6ba0cf2407f9864d7c52fd
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-27 00:52:56 +0600

    class8done

--- class8.js
+++ class8.js
@@ -5,8 +5,36 @@ function multiply (num1, num2){
         const mult = num1 * num2;
         return mult;
     }
+    else{
+        return 'give a number please'
+    }
 }
 const result = multiply(4,4);
 console.log(result)
 // let a = 5;
 // console.log(typeof a)
+function fullname(a, b){
+    if(typeof a && typeof b === 'string'){
+        let full = a + ' ' + b;
+        return full
+
+    }
+    else{
+        return 'give valid info'
+    }
+}
+console.log(fullname('a'))
+
+function getPrice(product){
+    if(typeof product === 'object'){
+
+        const price = product.price;
+        return price;
+    }
+    else{
+        return 'you should give a proper object'
+    }
+}
+console.log(getPrice({name: 'molom', price: 500}))
+// console.log(getPrice(['price500']))
+// console.log(getPrice(5))
